1e56c20f5c10aae1d5564d53aee7623c7c0a42f9
Matthew Landauer <mlandauer@gmail.com>
2019-05-02 17:11:52 +1000

    Always save record as planningalerts now does versioning

--- scraper.rb
+++ scraper.rb
@@ -25,12 +25,8 @@ def scrape_page(page, info_url, comment_url)
       "comment_url" => comment_url
     }
 #     p record
-    if (ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true)
-      puts "Saving record " + record['council_reference'] + ", " + record['address']
-      ScraperWiki.save_sqlite(['council_reference'], record)
-    else
-      puts "Skipping already saved record " + record['council_reference']
-    end
+    puts "Saving record " + record['council_reference'] + ", " + record['address']
+    ScraperWiki.save_sqlite(['council_reference'], record)
   end
 end
 
